d3f87838069bb000b173c9c6e5baa31218f3bec2
Ghost404 <djoaquimnamueto@gmail.com>
2025-11-02 11:02:35 +0200

    Update database.py

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ from config import DATABASE_URL
 
 
 # Ensure we use SQLAlchemy-friendly scheme (config.py normalizes postgres:// -> postgresql://)
-SQLALCHEMY_DATABASE_URL = "postgres://x2pypKr81B0c:vCyJUsHVYeHhfWHCsf692YnrTTTEldZIIDaBmWYX8lbwz28wwsHR2eGFpH9lHoO8e8cdOnHwU17A4YM3CIjN@o-destaque-y7cj-e81e00bf.app.indiecloud.dev/production"
+SQLALCHEMY_DATABASE_URL =DATABASE_URL
 
 # Use pool_pre_ping so SQLAlchemy checks connections before using them (helps with dropped connections)
 engine_kwargs = {
@@ -33,4 +33,5 @@ def get_db():
         yield db
     finally:
         db.close()
+
 
